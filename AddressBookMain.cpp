e38f227d09c8044c50d83aabded1d98331b4eadd
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-31 22:45:04 +0530

    ADDED Provision to chosse one of the task.

--- AddressBookMain.cpp
+++ AddressBookMain.cpp
@@ -1,26 +1,46 @@
-#include <iostream>
 #include "AddressBookDetails.cpp"
-#include "UserInputs.cpp"
 
 using namespace std;
 
-void addDetails()
+void presentChoice()
 {
     AddressBook addressBook;
     UserInput userInput;
-    string firstName = userInput.getFirstName();
-    string lastName = userInput.getLastName();
-    string address = userInput.getAddress();
-    string city = userInput.getCity();
-    string state = userInput.getState();
-    int zipcode = userInput.getZipCode();
-    string phoneNumber = userInput.getPhoneNumber();
-    Person person(firstName, lastName, address, city, state, zipcode, phoneNumber);
-    addressBook.addPersonDetails(person);
-    addressBook.display();
+    
+    while (true)
+    {
+        int choice;
+        cout << "\nSelect & enter the task you want to do: \n1: Add details \n2: Edit Person Details \n3: Display details. \n4: Exit" << endl;
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                {
+                    string firstName = userInput.getFirstName();
+                    string lastName = userInput.getLastName();
+                    string address = userInput.getAddress();
+                    string city = userInput.getCity();
+                    string state = userInput.getState();
+                    int zipcode = userInput.getZipCode();
+                    string phoneNumber = userInput.getPhoneNumber();
+                    Person person(firstName, lastName, address, city, state, zipcode, phoneNumber);
+                    addressBook.addPersonDetails(person);
+                }
+                break;
+            case 2:
+                addressBook.editPersonDetails();
+                break;
+            case 3:
+                addressBook.display();
+                break;
+            case 4:
+                exit(0);
+        }
+    }
 }
 
 int main()
 {
-    addDetails();
+    presentChoice();
 }
