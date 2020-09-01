1397a13cebf7f16010f62fdc14e2376e203a91b0
“Aniket_D” <aniketdakhare@gmail.com>
2020-09-01 12:19:34 +0530

    REFACTORED Code by removing parameterized constructor.

--- Person.cpp
+++ Person.cpp
@@ -12,28 +12,4 @@ class Person
         string state;
         string zipCode;
         string phoneNumber;
-
-        Person(){}
-
-        ~Person()
-        {
-            firstName.clear();
-            lastName.clear();
-            address.clear();
-            city.clear();
-            state.clear();
-            zipCode.clear();
-            phoneNumber.clear();
-        }
-
-        Person(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber)
-        {
-            this->firstName=firstName;
-            this->lastName=lastName;
-            this->address=address;
-            this->city=city;
-            this->state=state;
-            this->zipCode=zipCode;
-            this->phoneNumber=phoneNumber;
-        }
 };
